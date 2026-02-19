efea63646307f4eb41ab6ffb2efecb8d7087a203
chbl24 <chbl24@student.bth.se>
2026-02-19 17:00:28 +0100

    små fixar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from connector import mydb, mycursor
-
+from faker import Faker
 
 #mycursor.execute("drop table if exists users")
 
--- setup_db.py
+++ setup_db.py
@@ -10,35 +10,35 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS Customers "
 #Creates table for Transaction
 mycursor.execute("CREATE TABLE IF NOT EXISTS Transaction "
 "(Transaction_id INT AUTO_INCREMENT PRIMARY KEY, " \
-"Sold_date VARCHAR(255), " \
-"Customer_id INT, "\
+"Sold_date DATE NOT NULL, " \
+"Customer_id INT NOT NULL, "\
 "FOREIGN KEY (Customer_id) "
 "REFERENCES Customers(Customer_id))")
 
 #Creates table for Product
 mycursor.execute("CREATE TABLE IF NOT EXISTS Product "
 "(Product_id INT AUTO_INCREMENT PRIMARY KEY, " \
-"Category VARCHAR(60), " \
-"Name VARCHAR(60))")
+"Category VARCHAR(60) NOT NULL, " \
+"Name VARCHAR(60) NOT NULL)")
 
 #Creates table for Batch
 mycursor.execute("CREATE TABLE IF NOT EXISTS Batch "
 "(Batch_id INT AUTO_INCREMENT PRIMARY KEY, " \
 "Manufacturer VARCHAR(255), " \
-"Quantity INT, " \
+"Quantity INT NOT NULL, " \
 "Expiry_Date DATE, " \
-"Purchase_Price INT, " \
-"Product_id INT, " \
+"Purchase_Price INT NOT NULL, " \
+"Product_id INT NOT NULL, " \
 "FOREIGN KEY (Product_id) "
 "REFERENCES Product(Product_id))")
 
 #Creates table for Transaction_Item
 mycursor.execute("CREATE TABLE IF NOT EXISTS Transaction_Item "
-"(Quantity_Sold INT, " \
-"Sale_Price INT, " \
+"(Quantity_Sold INT NOT NULL, " \
+"Sale_Price INT NOT NULL, " \
 "Discount_Applied INT, " \
-"Transaction_id int, " \
-"Batch_id int, " \
+"Transaction_id int NOT NULL, " \
+"Batch_id int NOT NULL, " \
 "PRIMARY KEY(Transaction_id, Batch_id), " \
 "FOREIGN KEY (Transaction_id) "
 "REFERENCES Transaction(Transaction_id), " \
