640cd4c0fac033b516053a2f7b14f1cfdf47ad27
chbl24 <chbl24@student.bth.se>
2026-02-21 15:04:17 +0100

    main fix and query addition
    tweaked the data generator a bit and added a query to main

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from connector import mydb, mycursor
+from tabulate import tabulate
 
-#mycursor.execute("drop table if exists users")
+mycursor.execute(
+    "select p.Name, sum(b.Quantity) as Total_Stock " \
+    "from Product p " \
+    "join Batch b on p.Product_id = b.Product_id " \
+    "group by p.name"
+)
 
-
-mydb.commit() 
+results = mycursor.fetchall()
+print(tabulate(results, headers=["Produktnamn", "Totalt i lager"], tablefmt="grid"))
